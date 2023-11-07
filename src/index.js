e7f36e8b02431d2e4d71126cfb06af35d07bc663
Tessy <tessydominic48@gmail.com>
2023-11-07 13:53:41 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,168 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Hello React!!!</h1>;
+  return (
+    <div className="container">
+      {/* <h1>Hello React!!!!</h1> */}
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
+function Header() {
+  //   const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+  //The below line represents inline styling
+  //   return <h1 style={{color: "red", fontSize: "48px", textTransform: "uppercase"}}>Fast React Pizza Co.</h1>;
+  return (
+    //both styles of header and footer will be applied here
+    <header className="header footer">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  //parent component to pizza which is child component
+  const pizzas = pizzaData;
+  const numPizza = pizzas.length;
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      {numPizza > 0 ? (
+        //the below <> is a react fragment which will prevent wrapping of p and ul and keep it as 2 different boxes
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
+      ) : (
+        <p>We're still working on our menu please come back later</p>
+      )}
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12} //if you want to pass somthing thats not a string we use this method
+        photoName="pizzas/funghi.jpg"
+      /> */}
+    </main>
+  );
+}
+//to avoid using props.pizzaobject.name we pass pizzaobject (destructuring)
+function Pizza({ pizzaObject }) {
+  // pass prop into component and recieve it in the component you want it to be
+  //   console.log(props);
+  //   if (props.pizzaObject.soldOut) return null;
+  return (
+    <div className={`pizza ${pizzaObject.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
+      <div>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        {/* {pizzaObject.soldOut ? (
+          <span>Sold Out</span>
+        ) : (
+          <span>{pizzaObject.price}</span>
+        )} */}
+        <span>{pizzaObject.soldOut ? "SOLD OUT" : pizzaObject.price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  //   return React.createElement("footer", null, "We're currently open");
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 24;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  //   if (hour >= openHour && hour <= closeHour) alert("we are currently open");
+  //   else alert("sorry");
+  console.log(isOpen);
+  //   if (!isOpen) return <p>Closed</p>;
+  return (
+    <footer className="footer">
+      {/* {new Date().toLocaleTimeString()}. We're currently open */}
+      {isOpen ? (
+        <Order closeHour={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour} and {closeHour}
+        </p>
+      )}
+    </footer>
+  );
+}
+function Order({ closeHour }) {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us online</p>
+      <button className="btn">order</button>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
